a8ea887f0840778da03a84cfbf73cbabb213133b
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-31 16:09:37 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
+// Consegna 
+// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro. 
+
+
 const gridElement = document.getElementById('grid');
 
 const createGridElement = () => {
     const node = document.createElement('div');
-    node.classList.add('square');
+    node.classList.add('square-facile');
+    node.classList.add('numeri');
     return node;
 }
 
-
-for (let i = 1; i<=64; i++) {
+for (let i = 1; i<=100; i++) {
+    console.log(i)
+    
 
     const node = createGridElement();
-
-    //gridElement.innerHTML += `<div class="square"></div>`;
 
     node.addEventListener('click', function() {
         console.log(this);
         this.classList.add('clicked');
-    });
-
-    /*
-    node.addEventListener('click', () => {
-        console.log(this);
-        node.classList.add('clicked');
-    })
-    */
-
+        }
+    );
     gridElement.appendChild(node);
 
 }
